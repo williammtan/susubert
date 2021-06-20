a25cb2e087d1d167f879a119d7dc95c11746fdec
WilliamTan2704 <william.h.tan27@gmail.com>
2021-06-20 14:39:05 +0700

    created blocker

---
+++ blocking/blocker.py
@@ -0,0 +1,57 @@
+from utils.index import make_index, load_index
+from sentence_transformers import SentenceTransformer
+from preprocessing.serialize import serialize_products
+import pandas as pd
+import numpy as np
+import argparse
+import os
+
+def index_sbert(sbert, products):
+    sents = serialize_products(products, features=['name'])
+    embeddings = sbert.encode(sents, convert_to_numpy=True)
+    index, ids_mapping = make_index(embeddings, products.id)
+
+    return index
+
+def blocker(args):
+    products = pd.read_csv(args.products)
+
+    sbert = SentenceTransformer(args.model)
+    if not os.path.isfile(args.save_index):
+        index = index_sbert(sbert, products)
+        index.save(args.save_index)
+    else:
+        index = load_index(args.save_index, dimensions=768)
+
+    match_candidates = []
+
+    for i, prod in products.iterrows():
+        nn = np.array(index.get_nns_by_item(i, n=args.top_k, include_distances=True)).T
+        if args.threshold:
+            nn = nn[[nn[:, 1] > args.threshold]]
+        nn_ids = products[products.index.isin(nn[:, 0])].id.values
+        for id in nn_ids:
+            match_candidates.append((prod.id, id))
+    
+    match_candidates = np.array(match_candidates, )
+    match_candidates.sort()
+    match_candidates = np.unique(match_candidates, axis=0)
+    print(match_candidates)
+    
+    output = open(args.output, 'w')
+    output.write('id1, id2\n')
+    for match in match_candidates:
+        output.write(f'{products[products.id == match[0]]["name"].iloc[0]}, {products[products.id == match[1]]["name"].iloc[0]} \n')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--products')
+    parser.add_argument('--model')
+    parser.add_argument('--top-k', type=int, default=30)
+    parser.add_argument('--threshold', type=float)
+    parser.add_argument('--save-index')
+    parser.add_argument('--output')
+    args = parser.parse_args()
+
+    blocker(args)
--- preprocessing/serialize.py
+++ preprocessing/serialize.py
@@ -6,18 +6,43 @@
 import pandas as pd
 import argparse
 
-def serialize(args):
-    matches = pd.read_csv(args.matches)
-
+def serialize_products(products, features):
     serialized = []
-    for i, match in matches.iterrows():
-        serialized_matches = [' '.join(['COL ' + col + ' VAL ' + match[col+str(i)] for col in args.keep_columns]) for i in range(1,3)]
+    for i, match in products.iterrows():
+        serialized_matches = ' '.join(['COL ' + col + ' VAL ' + match[col] for col in features])
 
         serialized.append(serialized_matches)
     
+    return serialized
+
+def serialize_matches(matches, features):
+    serialized = []
+    for i, match in matches.iterrows():
+        serialized_matches = [' '.join(['COL ' + col + ' VAL ' + match[col+str(i)] for col in features]) for i in range(1,3)]
+
+        serialized.append(serialized_matches)
+    
+    return serialized
+
+def serialize(args):
+    dataset = pd.read_csv(args.matches)
     f = open(args.output, 'w')
-    for (match_str), is_match in zip(serialized, matches.match.values):
-        f.write(match_str[0] + '\t' + match_str[1] + '\t' + str(is_match) + '\n')
+
+    if 'match' in dataset.columns:
+        sent_pairs = serialize_matches(dataset, features=args.keep_columns)
+        for match_pair, is_match in zip(sent_pairs, dataset.match.values):
+            f.write(match_pair[0] + '\t' + match_pair[1] + '\t' + str(is_match) + '\n')
+
+    else:
+        sents = serialize_products(dataset, features=args.keep_columns)
+        for serialized_sent, id in zip(sents, dataset.id):
+            f.write(id + '\t' + serialized_sent + '\n')
+
+
+
+    
+    
+    
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
