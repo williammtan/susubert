5c5d46bf24ef3c049bb9ea24820b84a18abfa604
WilliamTan2704 <william.h.tan27@gmail.com>
2021-06-21 11:55:11 +0700

    converted feature_extraction to tf and np

--- preprocessing/feature_extraction.py
+++ preprocessing/feature_extraction.py
@@ -1,9 +1,9 @@
-import torch
 import argparse
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
-from transformers import BertTokenizer, AutoModel
+from transformers import BertTokenizer, TFAutoModel
+import tensorflow as tf
 
 from utils.index import make_index
 
@@ -14,37 +14,38 @@ def batch(iterable, n=1):
 
 def mean_pooling(model_output, attention_mask):
     token_embeddings = model_output[0]
-    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-    sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
-    sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+    input_mask_expanded = np.broadcast_to(np.expand_dims(attention_mask, axis=-1), token_embeddings.shape)
+    sum_embeddings = np.sum(token_embeddings * input_mask_expanded, 1)
+    sum_mask = np.clip(input_mask_expanded.sum(1), a_min=1e-9, a_max=np.inf)
     return sum_embeddings / sum_mask
 
 
-def feature_extraction(model, tokenizer, sentences, device, batch_size=1000):
+def feature_extraction(model, tokenizer, sentences, batch_size=1000):
     def get_embeddings(sentences):
-        encoded_input = tokenizer(sentences, padding=True, truncation=True, max_length=128, return_tensors='pt').to(device)
+        encoded_input = tokenizer(sentences, padding=True, truncation=True, max_length=128)
+        dataset = tf.data.Dataset.from_tensor_slices((
+            dict(encoded_input),
+        ))
 
-        with torch.no_grad():
-            model_output = model(**encoded_input)
+        model_output = model.predict(dataset.batch(32))
 
         sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
         return sentence_embeddings
 
     embedding = np.empty((0,768), int)
     for names in tqdm(batch(sentences, 1000), total = len(sentences.name) // batch_size):
-        embedding = np.append(embedding, get_embeddings(names.tolist()).cpu().detach().numpy(), axis=0)
+        embedding = np.append(embedding, get_embeddings(names.tolist()), axis=0)
     
     return embedding
         
 def make_features_index(args):
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     tokenizer = BertTokenizer.from_pretrained(args.model)
-    model = AutoModel.from_pretrained(args.model).to(device)
+    model = TFAutoModel.from_pretrained(args.model)
 
     products = pd.read_csv(args.products)
-    feature_embedding= feature_extraction(model, tokenizer, products.name, device)
+    feature_embedding= feature_extraction(model, tokenizer, products.name)
 
-    index, ids_mapping = make_index(feature_embedding, products.id.values)
+    index, _ = make_index(feature_embedding, products.id.values)
     index.save(args.save)
 
 if __name__ == '__main__':
