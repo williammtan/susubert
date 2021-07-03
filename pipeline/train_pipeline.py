439cc9984fc8ec2ce9a43fa485cac6bb149083c2
WilliamTan2704 <william.h.tan27@gmail.com>
2021-07-03 14:55:23 +0700

    added component.yaml specification for feature_extraction

--- pipeline/train_pipeline.py
+++ pipeline/train_pipeline.py
@@ -1,21 +1,18 @@
 import kfp
 from kfp import dsl
-from kfp.components import func_to_container_op, load_component_from_url, OutputPath, InputPath
-
-def read_df(path : InputPath(str), output_path: OutputPath(str)) -> None:
-    import pandas as pd
-    df = pd.read_csv(path)
-    print(df)
-    df.to_csv(output_path)
+from kfp.components import load_component_from_url, load_component_from_file
 
 @dsl.pipeline(name='train pipeline')
-def train_pipeline():
-    read_df_op = func_to_container_op(func=read_df, packages_to_install=['pandas'])
-    download = load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/0795597562e076437a21745e524b5c960b1edb68/components/google-cloud/storage/download/component.yaml')
+def train_pipeline(
+    lm:str='indobenchmark/indobert-base-p1',
+    products:str='gs://ml_foodid_project/product-matching/pareto_training.csv'
+):
+    download_op = load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/0795597562e076437a21745e524b5c960b1edb68/components/google-cloud/storage/download/component.yaml')
+    feature_extraction_op = load_component_from_file('feature_extraction/component.yaml')
 
-    download_task = download('gs://data_external_backup/EXTERNAL_PRODUCTS.csv')
+    download_task = download_op(products)
 
-    read_df_op(download_task.output)
+    feature_extraction_op(lm, download_task.output)
 
 if __name__ == '__main__':
     # kfp.compiler.Compiler().compile(train_pipeline, 'train_pipline.yaml')
