de1325b6bd8947bde98777663589d204d23e79ca
mlkulina <dev-ml@kulina.id>
2021-07-05 09:03:51 +0000

    added train_blocker component

---
+++ pipeline/train_blocker/dataset.py
@@ -0,0 +1,55 @@
+import torch
+from torch.utils.data import Dataset
+from sentence_transformers import InputExample
+from transformers import AutoTokenizer
+import pandas as pd
+import numpy as np
+import os
+
+class SBertMatcherDataset(Dataset):
+    def __init__(self, dataset, labels=None, lm=None, max_len=150):
+        """
+        Inputs:
+            dataset: str (to file) or list
+            labels: list of labels
+        """
+        if type(dataset) == str:
+            self.sents, self.labels = self.read_data(dataset)
+        else:
+            self.sents = dataset
+            self.labels = labels
+        
+        if labels is not None:
+            assert len(self.sents) == len(self.labels), "Length of sents and labels don't match"
+
+        self.lm = lm
+        self.max_len = max_len
+        self.tokenizer = AutoTokenizer.from_pretrained(self.lm)
+    
+    def read_data(self, fname):
+        file_extension = os.path.splitext(fname)[1]
+        sents = []
+        labels = []
+
+        if file_extension == '.txt':
+            for row in open(fname):
+                cols = row.split('\t')
+                if len(cols) >= 2:
+                    sents.append(cols[:2])
+                    if len(cols) == 3:
+                        labels.append(int(cols[-1].replace('\n','')))
+        elif file_extension == '/csv':
+            pd.read_csv()
+
+        return sents, labels
+    
+    def __len__(self):
+        return len(self.sents)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        
+        x = InputExample(texts=self.sents[idx], label=float(self.labels[idx]))
+
+        return x
---
+++ pipeline/train_blocker/train_blocker.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import argparse
+
+from sentence_transformers import SentenceTransformer, models, losses
+from sentence_transformers.evaluation import BinaryClassificationEvaluator
+from dataset import SBertMatcherDataset
+from torch.utils.data import DataLoader
+
+from sklearn.model_selection import train_test_split
+
+VAL_SPLIT=0.1 # TODO: make this a parameter
+
+def create_sbert_model(lm, max_length=64):
+    word_embedding_model = models.Transformer(lm, max_seq_length=max_length)
+    pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension())
+
+    return SentenceTransformer(modules=[word_embedding_model, pooling_model])
+
+def train_blocker(lm, train_matches, val_matches, args):
+    model = create_sbert_model(lm)
+
+    dataset = SBertMatcherDataset(np.array([train_matches.sent1, train_matches.sent2]).T, labels=train_matches.match, lm=lm)
+    dataloader = DataLoader(dataset, shuffle=True, batch_size=args.batch_size)
+    loss_func = losses.CosineSimilarityLoss(model)
+
+    val_evaluator = BinaryClassificationEvaluator(sentences1=val_matches.name1.tolist(), val_matches=matches.name2.tolist(), labels=matches.match.tolist())
+
+    model.fit(train_objectives=[(dataloader, loss_func)], epochs=args.n_epochs, evaluator=val_evaluator)
+
+    return model
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--matches')
+    parser.add_argument('--lm')
+    parser.add_argument('--batch-size', type=int)
+    parser.add_argument('--lr', type=float)
+    parser.add_argument('--n-epochs', type=int)
+    parser.add_argument('--save-model')
+    args = parser.parse_args()
+
+    matches = pd.read_csv(args.matches)
+    train_matches, val_matches = train_test_split(matches, test_size=VAL_SPLIT)
+
+    model = train_blocker(args.lm, train_matches, val_matches, args)
+    model.save(args.save_model)
