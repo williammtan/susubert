d97ec4078dcc4bcac2043ac5071eb14334203667
mlkulina <dev-ml@kulina.id>
2021-07-05 06:52:46 +0000

    added upload component to pipeline

--- pipeline/train_pipeline.py
+++ pipeline/train_pipeline.py
@@ -8,6 +8,7 @@ from utils import preprocess, train_test_split
 def train_pipeline(
     lm:str='indobenchmark/indobert-base-p1',
     products:'URI'='gs://ml_foodid_project/product-matching/susubert/pareto_training.csv', # type: ignore
+    model_save: str='gs://ml_foodid_project/product-matching/susubert/pareto_model',
     keep_columns: list=['name', 'price'],
     batch_size: int=32,
     learning_rate: float=3e-5,
@@ -21,6 +22,7 @@ def train_pipeline(
     train_test_split_op = func_to_container_op(func=train_test_split, packages_to_install=['pandas', 'sklearn'])
     train_op = load_component_from_file('train/component.yaml')
     evaluate_op = load_component_from_file('evaluate/component.yaml')
+    upload_op = load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/master/components/google-cloud/storage/upload_to_explicit_uri/component.yaml')
 
     # download and simple preprocess
     download_task = download_op(products)
@@ -31,8 +33,11 @@ def train_pipeline(
     batch_selection_task = batch_selection_op(preprocess_task.output, feature_extraction_task.output).set_gpu_limit(1)
     serialize_op = serialize_op(batch_selection_task.output, preprocess_task.output, keep_columns).set_gpu_limit(1)
     train_test_split_task = train_test_split_op(serialize_op.output)
+
+    # training
     train_task = train_op(train_test_split_task.outputs['train'], lm, batch_size, learning_rate, num_epochs).set_gpu_limit(1)
     evaluate_task = evaluate_op(train_test_split_task.outputs['test'], lm, train_task.output, batch_size)
+    upload_task = upload_op(train_task.output, model_save)
 
 
 if __name__ == '__main__':
