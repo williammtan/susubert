7eb841de5157ad8c35cedb8db7637d2a0e9b8c84
mlkulina <dev-ml@kulina.id>
2021-07-09 07:54:05 +0000

    updated match pipeline to use rds db and added components to cache matches

--- pipeline/match_pipeline.py
+++ pipeline/match_pipeline.py
@@ -1,66 +1,68 @@
 import kfp
 from kfp import dsl
 from kfp.components import func_to_container_op, load_component_from_url, load_component_from_file
-import sys
 
-from utils import preprocess, fin
+from utils import preprocess, query_rds, save_clusters, fin, drop_cache_matches, merge_cache_matches
 
-@dsl.pipeline(name='train pipeline')
+@dsl.pipeline(name='matcher pipeline')
 def match_pipeline(
-    lm:str='indobenchmark/indobert-base-p1',
-    products:'URI'='gs://ml_foodid_project/product-matching/susubert/pareto_training.csv', # type: ignore
+    lm: str='indobenchmark/indobert-base-p1',
     model_save: 'URI'='gs://ml_foodid_project/product-matching/susubert/pareto_model/model', # type: ignore
-    keep_columns: list=['name', 'price'],
-
     batch_size: int=32,
+    sbert_model: 'URI', # type: ignore
 
-    blocker_epochs: int=1,
-    blocker_learning_rate: float=2e-5,
-    blocker_threshold: float=0.5,
-    blocker_top_k: int=50,
+    product_query: str="""
+    SELECT *
+    FROM external_temp_products_pareto
+    WHERE id IN (SELECT id FROM master_product_clusters WHERE master_product_status_id = 3) OR id NOT IN (SELECT id FROM master_product_clusters)
+    """,
+    
+    keep_columns: list=['name', 'price'],
+    blocker_top_k: int=100,
+    blocker_threshold: float=0.25,
 
     match_threshold: float=0.8,
-    match_save: str='gs://ml_foodid_project/product-matching/susubert/pareto_training_results.csv', # type: ignore
-    min_cluster_size: int=3
+    min_cluster_size: int=3,
+
+    cache_matches_table: str="matches_cache",
 ):
+    """This pipeline will block matches and predict product matches (using cache) to create clusters."""
+
     download_op = load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/0795597562e076437a21745e524b5c960b1edb68/components/google-cloud/storage/download/component.yaml')
     preprocess_op = func_to_container_op(func=preprocess, packages_to_install=['pandas'])
-    feature_extraction_op = load_component_from_file('feature_extraction/component.yaml') 
-    batch_selection_op = load_component_from_file('batch_selection/component.yaml')
+    query_rds_op = func_to_container_op(func=query_rds, packages_to_install=['sqlalchemy', 'pandas', 'pymysql'])
     serialize_op = load_component_from_file('serialize/component.yaml')
-    train_blocker_op = load_component_from_file('train_blocker/component.yaml')
     blocker_op = load_component_from_file('blocker/component.yaml')
+    drop_cache_op = func_to_container_op(func=drop_cache_matches, packages_to_install=['sqlalchemy', 'pandas', 'pymysql'])
     matcher_op = load_component_from_file('matcher/component.yaml')
+    merge_cache_op = func_to_container_op(func=merge_cache_matches, packages_to_install=['pandas'])
     fin_op = func_to_container_op(func=fin, packages_to_install=['python-igraph', 'numpy', 'pandas'])
-    upload_op = load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/master/components/google-cloud/storage/upload_to_explicit_uri/component.yaml')
+    save_clusters_op = func_to_container_op(func=save_clusters, packages_to_install=['sqlalchemy', 'pandas', 'pymysql'])
 
-    # download and simple preprocess
-    download_task = download_op(products)
-    preprocess_task = preprocess_op(download_task.output)
-
-    # preprocessing
-    feature_extraction_task = feature_extraction_op(lm, preprocess_task.outputs['master_products']).set_gpu_limit(1)
-    batch_selection_task = batch_selection_op(preprocess_task.outputs['master_products'], feature_extraction_task.output).set_gpu_limit(1)
-    serialize_task = serialize_op(matches=batch_selection_task.output, products=preprocess_task.outputs['master_products'], keepcolumns=keep_columns).set_gpu_limit(1)
-    serialize_products_task = serialize_op(matches='', products=preprocess_task.outputs['products'], keepcolumns=keep_columns).set_gpu_limit(1)
-
-    # blocking
-    train_blocker_task = train_blocker_op(serialize_task.output, lm, batch_size, blocker_learning_rate, blocker_epochs).set_gpu_limit(1)
-    blocker_task = blocker_op(preprocess_task.outputs['products'], serialize_products_task.output, train_blocker_task.output, blocker_top_k, blocker_threshold).set_gpu_limit(1)
-    serialize_blocker_task = serialize_op(matches=blocker_task.output, products=preprocess_task.outputs['products'], keepcolumns=keep_columns).set_gpu_limit(1)
-
-    # matching
+    download_sbert_task = download_op(sbert_model)
     download_model_task = download_op(model_save)
-    matcher_task = matcher_op(matches=serialize_blocker_task.output, lm=lm, model=download_model_task.output, batchsize=batch_size, threshold=match_threshold).set_gpu_limit(1)
-    fin_task = fin_op(matches=matcher_task.output, products=preprocess_task.outputs['products'], min_cluster_size=min_cluster_size)
-    upload_task = upload_op(fin_task.output, match_save)
 
+    query_products = query_rds_op(query=product_query)
+    preprocess_task = preprocess_op(query_products.output)
 
+    serialize_products_task = serialize_op(matches='', products=preprocess_task.outputs['products'], keepcolumns=keep_columns).set_gpu_limit(1)
+    blocker_task = blocker_op(preprocess_task.outputs['products'], serialize_products_task.output, download_sbert_task.output, blocker_top_k, blocker_threshold).set_gpu_limit(1)
 
+    if cache_matches_table is not None:
+        query_cache_matches = query_rds_op(query=f"SELECT * FROM {cache_matches_table} WHERE model = {model_save.split('/')[-1]}") # find matches where the model is the inputted model
+        drop_cache_task = drop_cache_op(blocked_matches=blocker_task.output, cached_matches=query_cache_matches.output)
+        serialize_matches_task = serialize_op(matches=drop_cache_task.output, products=preprocess_task.outputs['products'], keepcolumns=keep_columns).set_gpu_limit(1)
+    else:
+        serialize_matches_task = serialize_op(matches=blocker_task.output, products=preprocess_task.outputs['products'], keepcolumns=keep_columns).set_gpu_limit(1)
 
-if __name__ == '__main__':
-    if sys.argv[1] == 'compile':
-        kfp.compiler.Compiler().compile(match_pipeline, 'match_pipeline.yaml')
-    elif sys.argv[1] == 'run':
-        client = kfp.Client(host='https://118861cf2b92c13d-dot-us-central1.pipelines.googleusercontent.com')
-        client.create_run_from_pipeline_func(match_pipeline, arguments={})
+    matcher_task = matcher_op(matches=serialize_matches_task.output, lm=lm, model=download_model_task.output, batchsize=batch_size, threshold=match_threshold).set_gpu_limit(1)
+
+    if cache_matches_table is not None:
+        merge_cache_task = merge_cache_op(matches=matcher_task.output, cache_matches=query_cache_matches.output)
+        fin_task = fin_op(matches=merge_cache_task.output, products=preprocess_task.outputs['products'], min_cluster_size=min_cluster_size).set_gpu_limit(1)
+    else:
+        fin_task = fin_op(matches=matcher_task.output, products=preprocess_task.outputs['products'], min_cluster_size=min_cluster_size).set_gpu_limit(1)
+
+    save_clusters_task = save_clusters_op(clusters=fin_task.output)
+
+    
--- pipeline/utils.py
+++ pipeline/utils.py
@@ -89,3 +89,101 @@ def query_rds(
 
     Path(save_query_path).parent.mkdir(parents=True, exist_ok=True)
     df.to_csv(save_query_path, index=False)
+
+def save_to_rds(
+    dataframe_path: InputPath(str),
+    table: str,
+    if_exists: str='append',
+    index: bool=False
+    ):
+    from sqlalchemy import create_engine
+    import pandas as pd
+
+    db_connection_str = os.environ['sql_endpoint']
+    db_connection = create_engine(db_connection_str)
+    db_connection.connect()
+
+    df = pd.read_csv(dataframe_path)
+    df.to_sql(table, db_connection, schema='food', if_exists=if_exists, index=index)
+
+def drop_cache_matches(
+    blocked_matches_path: InputPath(str),
+    cached_matches_path: InputPath(str),
+    match_candidates_path: OutputPath(str)
+    ):
+    import pandas as pd
+
+    matches = pd.read_csv(blocked_matches_path)
+    cached_matches = pd.read_csv(cached_matches_path)
+
+    match_candidates = matches[~matches.isin(cached_matches)] # find matches that have NOT been cached
+
+    match_candidates.to_csv(match_candidates_path, index=False)
+
+def merge_cache_matches(
+    matches_path: InputPath(str),
+    cached_matches_path: InputPath(str),
+    matches_output_path: OutputPath(str)
+    ):
+    import pandas as pd
+    from pathlib import Path
+
+    df = pd.concat([matches_path, cached_matches_path])
+
+    Path(matches_output_path).parent.mkdir(parents=True, exist_ok=True)
+    df.to_csv(matches_output_path, index=False)
+
+def save_clusters(
+    clusters_path: InputPath(str),
+    products_path: InputPath(str)
+    ):
+    from sqlalchemy import create_engine, update
+    import pandas as pd
+
+    db_connection_str = os.environ['sql_endpoint']
+    db_connection = create_engine(db_connection_str)
+    db_connection.connect()
+
+    query_rds = lambda query: pd.read_sql(query, con=db_connection)
+
+    # load master products and clusters
+    df_mp = query_rds("SELECT id, name FROM food.master_products WHERE is_deleted=0").rename(columns={'id':'master_product_id', 'name':'master_product'})
+    df_mpc = query_rds("SELECT * FROM food.master_product_clusters WHERE master_product_status_id != 4") # select all mpc that are in use
+
+    # merge products with clusters
+    products = pd.read_csv(products_path)
+    df_cls = pd.read_csv(clusters_path).merge(products[['id', 'name', 'master_product']], how='left', on='id')
+    df_cls = df_cls.merge(df_mp, how="left", on="master_product") # merge with mps
+    df_cls.drop_duplicates(subset="id", inplace=True)
+
+    # add statuses
+    df_cls["master_product_status_id"] = 1 # unnamed cluster
+    df_cls.loc[df_cls.master_product_id.notnull(), "master_product_status_id"] = 2 # named cluster
+
+    # rename to db schema
+    df_cls = df_cls.rename(columns={"id":"product_source_id", "cluster":"cluster_id"})
+    df_cls = df_cls[["cluster_id", "product_source_id", "master_product_id", "master_product_status_id"]]
+
+    # change status of all the mpcs
+    stmt = (
+        update('master_product_clusters')
+        .values(master_product_status_id=4) # set all mpcs to NOT USED
+    )
+    with db_connection.begin() as conn:
+        conn.execute(stmt) # run update query
+
+    # append to db
+    df_cls.to_sql("master_product_clusters", db_connection, schema="food", if_exists="append", index=False)
+
+    # add to mp history
+    df_mph_current = df_mpc.copy()
+    df_mph_current["current_status_id"] = 4 # changed old clusters status id from UNNAMED OR NAMED CLUSTER to NOT USED
+
+    df_mph_new = df_cls.copy()
+    df_mph_new = df_mph_new["master_product_status_id"] # don't change status, meaning added new clusters
+
+    df_mph = pd.concat([df_mph_new, df_mph_new])
+    df_mph = df_mph.rename(columns={"id":"master_product_cluster_id", "master_product_status_id":"previous_status_id"})
+    df_mph.to_sql("master_product_status_histories", db_connection, schema="food", if_exists="append", index=False)
+
+
